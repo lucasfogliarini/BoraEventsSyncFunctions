6e45072d7f44311aa01c8fbc0804d4b724a5fe1c
Lucas Fogliarini Pedroso <lucas.pedroso@cogna.com.br>
2023-12-25 23:45:24 -0300

    Criação do Araujo Vianna e Teatro do Bourbon Country

---
+++ BoraEventsSyncFunctions/EventSync/TeatroBourbounCountryUhuuSync.cs
@@ -0,0 +1,40 @@
+using BoraCrawlers;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace BoraEventsSyncFunctions.EventSync
+{
+	public class TeatroBourbounCountryUhuuSync : EventSync
+    {
+        const string EVENTS_QUERY = "v/teatro-do-bourbon-country-72";
+
+        public TeatroBourbounCountryUhuuSync(ILoggerFactory loggerFactory) : base(new UhuuCrawler(EVENTS_QUERY))
+        {
+            _logger = loggerFactory.CreateLogger<TeatroBourbounCountryUhuuSync>();
+        }
+
+        [Function(nameof(TeatroBourbounCountryUhuuSync))]
+        [ServiceBusOutput("%AzureServiceBusEventCreatedQueue%", Connection = "AzureServiceBusConnectionString")]
+        public override async Task<IEnumerable<EventCreated>> RunAsync([TimerTrigger("%CrawlerCron%", RunOnStartup = false)] TimerInfo timer)
+        {
+			DateTime startDate = DateTime.Today;
+			DateTime endDate = DateTime.Today.AddMonths(6);
+            InitLog(startDate, endDate);
+
+			var events = await _boraCrawler.CrawlEventsAsync();
+
+			LogEvents(events);
+
+			return events
+                .Where(e => e.DateTime >= startDate && e.DateTime <= endDate)
+                .Select(e => new EventCreated
+                {
+                    Start = e.DateTime,
+                    Title = e.Title,
+                    EventLink = e.EventLink,
+                    Description = e.Price,
+                    Location = e.Location
+                });
+        }
+    }
+}
